271f916aca9ed891672834d8bbc6e8c99ed9730d
pradiplamsal152 <pradip.lamsal152@gmail.com>
2026-08-17 14:00:09 +0545

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 var builder = WebApplication.CreateBuilder(args);
+builder.WebHost.UseUrls("http://*:8080", "http://*:5000"); // Standard container ports
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello from GitHub CI/CD Pipeline! App version: 1.0.0");
